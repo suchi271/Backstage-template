b7e3424aa0fb406e6314a5404ba2bc3605ebcfe5
Akshay S Jaiswal <aksjaiswal@microsoft.com>
2024-04-25 21:10:40 +0530

    Added the authentication

--- packages/backend/src/plugins/auth.ts
+++ packages/backend/src/plugins/auth.ts
@@ -1,66 +1,3 @@
-// import {
-//   createRouter,
-//   providers,
-//   defaultAuthProviderFactories,
-// } from '@backstage/plugin-auth-backend';
-// import { Router } from 'express';
-// import { PluginEnvironment } from '../types';
-// import { DEFAULT_NAMESPACE, stringifyEntityRef } from '@backstage/catalog-model';
-
-// export default async function createPlugin(
-//   env: PluginEnvironment,
-// ): Promise<Router> {
-//   return await createRouter({
-//     logger: env.logger,
-//     config: env.config,
-//     database: env.database,
-//     discovery: env.discovery,
-//     tokenManager: env.tokenManager,
-//     providerFactories: {
-//       ...defaultAuthProviderFactories,
-
-//       github: providers.github.create({
-//         signIn: {
-//           resolver(_, ctx) {
-//             const userRef = 'user:default/guest'; // Must be a full entity reference
-//             return ctx.issueToken({
-//               claims: {
-//                 sub: userRef, // The user's own identity
-//                 ent: [userRef], // A list of identities that the user claims ownership through
-//               },
-//             });
-//           },
-//         },
-//       }),
-//       microsoft: providers.microsoft.create({
-//         signIn: {
-//           resolver({profile}, ctx) {
-//             if (!profile.email) {
-//               throw new Error(
-//                 'Login failed, user does not contain an email',
-//               );
-//             }
-//             const [localPart] = profile.email.split('@');
-
-//             const userEntityRef = stringifyEntityRef({
-//               kind: 'User',
-//               name: localPart,
-//               namespace: DEFAULT_NAMESPACE,
-//             });
-
-//             return ctx.issueToken({
-//               claims: {
-//                 sub: userEntityRef,
-//                 ent: [userEntityRef],
-//               }
-//             })
-//           },
-//           // resolver: providers.github.resolvers.usernameMatchingUserEntityName(),
-//         },
-//       }),
-//     },
-//   });
-// }
 import {
   createRouter,
   providers,
@@ -68,6 +5,7 @@ import {
 } from '@backstage/plugin-auth-backend';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
+import { DEFAULT_NAMESPACE, stringifyEntityRef } from '@backstage/catalog-model';
 
 export default async function createPlugin(
   env: PluginEnvironment,
@@ -81,12 +19,74 @@ export default async function createPlugin(
     providerFactories: {
       ...defaultAuthProviderFactories,
 
-      microsoft: providers.microsoft.create({ 
-        signIn: { 
-          resolver: 
-            providers.microsoft.resolvers.emailMatchingUserEntityAnnotation(), 
-        }, 
-      }), 
+      github: providers.github.create({
+        signIn: {
+          resolver(_, ctx) {
+            const userRef = 'user:default/guest'; // Must be a full entity reference
+            return ctx.issueToken({
+              claims: {
+                sub: userRef, // The user's own identity
+                ent: [userRef], // A list of identities that the user claims ownership through
+              },
+            });
+          },
+        },
+      }),
+      microsoft: providers.microsoft.create({
+        signIn: {
+          resolver({profile}, ctx) {
+            if (!profile.email) {
+              throw new Error(
+                'Login failed, user does not contain an email',
+              );
+            }
+            const [localPart] = profile.email.split('@');
+
+            const userEntityRef = stringifyEntityRef({
+              kind: 'User',
+              name: localPart,
+              namespace: DEFAULT_NAMESPACE,
+            });
+
+            return ctx.issueToken({
+              claims: {
+                sub: userEntityRef,
+                ent: [userEntityRef],
+              }
+            })
+          },
+          // resolver: providers.github.resolvers.usernameMatchingUserEntityName(),
+        },
+      }),
     },
   });
 }
+// import {
+//   createRouter,
+//   providers,
+//   defaultAuthProviderFactories,
+// } from '@backstage/plugin-auth-backend';
+// import { Router } from 'express';
+// import { PluginEnvironment } from '../types';
+
+// export default async function createPlugin(
+//   env: PluginEnvironment,
+// ): Promise<Router> {
+//   return await createRouter({
+//     logger: env.logger,
+//     config: env.config,
+//     database: env.database,
+//     discovery: env.discovery,
+//     tokenManager: env.tokenManager,
+//     providerFactories: {
+//       ...defaultAuthProviderFactories,
+
+//       microsoft: providers.microsoft.create({ 
+//         signIn: { 
+//           resolver: 
+//             providers.microsoft.resolvers.emailMatchingUserEntityAnnotation(), 
+//         }, 
+//       }), 
+//     },
+//   });
+// }
